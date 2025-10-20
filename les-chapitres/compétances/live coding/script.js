d7307c7b2be8cc4c52c684911fb2d4b1af206e15
tasnimesahli <tasnimsahli97@gmail.com>
2025-10-20 14:15:34 +0100

    Add files via upload

---
+++ les-chapitres/compétances/live coding/script.js
@@ -0,0 +1,14 @@
+function numberc() {
+    let table=["c1","c3","c1","c2","c4"];
+    let choisi="c1";
+    let compteur=0;
+    for (let i = 0; i<table.length; i++) {
+    if (table[i]===choisi) {
+        compteur++;
+    }
+
+ }
+   console.log("le nombre totale "+choisi +"est" +compteur);
+ 
+}
+numberc(); 
