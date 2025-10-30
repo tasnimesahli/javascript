1fec216b76770de8b207dfdfca08db55231a36d5
tasnimesahli <tasnimsahli97@gmail.com>
2025-10-30 11:54:50 +0100

    Delete Sélecteurs-DOM–getElementById-querySelector directory

--- Sélecteurs-DOM–getElementById-querySelector/script.js
+++
@@ -1,5 +0,0 @@
-let titre = document.getElementById("titre-principal");
-console.log("Texte du titre : " + titre.innerText);
-
-let bouton = document.querySelector(".btn");
-console.log("Texte du bouton : " + bouton.innerText);
